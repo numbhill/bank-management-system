862a80a144c323602867460bb3cdd9a164e3367d
numbhill <123810757+numbhill@users.noreply.github.com>
2024-01-11 14:15:52 +0100

    Swish Bank Signup2

---
+++ src/swishbank/management/system/Signup2.java
@@ -0,0 +1,13 @@
+package swishbank.management.system;
+
+import javax.swing.*;
+
+public class Signup2 extends JFrame {
+    Signup2(){
+        setSize(500,500);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new Signup2();
+    }
+}
